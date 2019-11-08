3b9d61c5b802b7030e4463233f1cf11c8ed25fb1
Pascal T. Hein <pascal.hein@gmx.de>
2019-11-08 17:47:44 +0100

    first tests of tei creation

---
+++ code/transform_all.sh
@@ -0,0 +1,7 @@
+for path_to_html in ../html/marx_engels/*/*; do
+    folder="$(dirname $path_to_html)"
+    folder="$(basename $folder)"
+    path_to_tei=../tei/marx_engels/$folder/$(basename "$path_to_html" .htm).xml 
+    python3 transform_to_tei.py $path_to_html $path_to_tei
+done;
+
