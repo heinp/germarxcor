4b8b78ac7ac0603d8742778e47f9557783f7a1e4
Pascal T. Hein <pascal.hein@gmx.de>
2020-05-04 13:31:40 +0200

    new txts created

--- code/json2txt.py
+++ code/json2txt.py
@@ -4,6 +4,7 @@ from collections import defaultdict
 from os import path
 
 in_files = glob.glob("../json/*/*")
+in_files = [file for file in in_files if "pl" not in file]
 orged_files = defaultdict(list)
 for file in in_files:
     author = path.basename(path.dirname(file))
@@ -12,13 +13,15 @@ for file in in_files:
 
 for author, files in orged_files.items():
     author_text = ""
+    token_count = 0
     for file in files:
         with open(file) as json_file:
             data = json.load(json_file)
         text = [item for sublist in data.values() for subsublist in sublist for item in subsublist]
-        print(text)
+        token_count += len(text)
         text = " ".join(text)
         author_text += text
+    print(author, token_count)
     with open(f"../txt/{author}.txt", "w") as out_file:
         out_file.write(author_text)
 
