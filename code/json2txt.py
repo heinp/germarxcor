bff69b757fe0267ffd95d6444bd45303528e5a03
Pascal T. Hein <pascal.hein@gmx.de>
2020-05-27 09:56:45 +0200

    minor corrections

--- code/json2txt.py
+++ code/json2txt.py
@@ -14,14 +14,17 @@ for file in in_files:
 for author, files in orged_files.items():
     author_text = ""
     token_count = 0
+    sent_count = 0
     for file in files:
         with open(file) as json_file:
             data = json.load(json_file)
-        text = [item for sublist in data.values() for subsublist in sublist for item in subsublist]
+        sents = [item for sublist in data.values() for item in sublist]
+        text = [item for sublist in sents for item in sublist]
+        sent_count += len(sents)
         token_count += len(text)
         text = " ".join(text)
         author_text += text
-    print(author, token_count)
-    with open(f"../txt/{author}.txt", "w") as out_file:
-        out_file.write(author_text)
+    print(author, sent_count)
+    #with open(f"../txt/{author}.txt", "w") as out_file:
+    #    out_file.write(author_text)
 
